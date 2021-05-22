b524c2123481f89f1a99bd693187e93613c98d04
csbustamatea <carlosbu96@hotmail.com>
2021-05-22 09:49:26 -0500

    Proyecto Final Unidad 2

---
+++ Script.js
@@ -0,0 +1,14 @@
+$('.menu').mouseover(function(){
+
+$(this).css('zoom','1.1').css('color','blue')
+})
+$('.menu2').mouseout(function(){
+
+    $(this).css('zoom','1').css('color','white')
+    })
+    
+function enviar(){
+
+    alert("Información enviada");
+    
+}
